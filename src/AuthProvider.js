eaea05a6cd97a5fe3144e4fd5494b36c66b640b6
mohammedBnomr <moohaboy@gmail.com>
2022-03-21 12:15:54 +0300

    last commit

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -11,6 +11,7 @@ export const AuthProvider = ({children}) => {
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             setCurrentUser(user)
+            setUsername(user.displayName)
             setPending(false)
         })
     }, [])
